6d6ddc9ee023712057c4dbaead8771ea626c8615
Yulong Tan <yulongtan67@gmail.com>
2019-03-28 10:25:03 -0700

    Replace test file with api call

--- clients/RestClient.js
+++ clients/RestClient.js
@@ -1,14 +1,8 @@
 let fetch = require('node-fetch');
 let base64 = require('base-64');
-let auth = require('./auth.json');
+let auth = require('../auth.json');
 
-let flightNumber = '380';
-let date = '2019-03-28';
-let origin = 'SEA';
-
-let url = `https://api.alaskaair.com/gds/1/flights/${flightNumber}/priorityList?date=${date}&origin=${origin}&asgds-appid=${auth.asgds-appid}`;
-
-async function Get() {
+async function Get(url) {
   let options = {
     headers: {
       'Authorization': `Basic ${base64.encode(`${auth.username}:${auth.password}`)}`
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 let _ = require('lodash');
 let fs = require('fs');
 
-let json = require('./test_file.json');
+let client = require('./clients/RestClient');
+let auth = require('./auth.json');
 
-let upgradeCodes = ['AUPG', 'AUPGT', 'DSRF', 'DSRFT', 'DS75', 'DS75T', 'DSRG', 'DSRGT', 'DSRM', 'DSRMT', 'DSP1', 'DSP2', 'DSP3', 'DSR', 'DSRT'];
+async function main() {
+  let flightNumber = '380';
+  let date = '2019-03-29';
+  let origin = 'SEA';
 
-let res = _.filter(json.priorityList, (person) => {
-  return !upgradeCodes.includes(person.priorityCode)
-})
+  let url = `https://api.alaskaair.com/gds/1/flights/${flightNumber}/priorityList?date=${date}&origin=${origin}&asgds-appid=${auth['asgds-appid']}`;
+  let json = await client.Get(url);
 
-fs.writeFileSync('results.json', JSON.stringify(res, null, 2), (err) => {
-  console.log(`Error during file save: ${err}`);;
-});
+  let upgradeCodes = ['AUPG', 'AUPGT', 'DSRF', 'DSRFT', 'DS75', 'DS75T', 'DSRG', 'DSRGT', 'DSRM', 'DSRMT', 'DSP1', 'DSP2', 'DSP3', 'DSR', 'DSRT'];
 
-console.log(`Number of objects: ${res.length}`);
+  let res = _.filter(json.priorityList, (person) => {
+    return !upgradeCodes.includes(person.priorityCode)
+  })
+
+  fs.writeFileSync('results.json', JSON.stringify(res, null, 2), (err) => {
+    console.log(`Error during file save: ${err}`);;
+  });
+
+  console.log(`Number of objects: ${res.length}`);
+}
+
+
+main();
